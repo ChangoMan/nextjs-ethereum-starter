e46094ced20cce64ddeb79085965208785a1c921
Hunter Chang <hchang@recurforever.com>
2022-09-06 21:43:56 -0600

    adding ipfs and fetch photos

--- frontend/pages/nft.tsx
+++ frontend/pages/nft.tsx
@@ -1,42 +1,121 @@
 import { Button, Heading } from '@chakra-ui/react'
+import { create } from 'ipfs-http-client'
 import type { NextPage } from 'next'
 import { useSession } from 'next-auth/react'
+import { useCallback, useEffect, useState } from 'react'
 import {
   useContractRead,
   useContractWrite,
   usePrepareContractWrite,
+  useWaitForTransaction,
 } from 'wagmi'
 import YourNFT from '../artifacts/contracts/YourNFT.sol/YourNFT.json'
 import { Layout } from '../components/layout/Layout'
+import { generateTokenUri } from '../utils/generateTokenUri'
+
+const CONTRACT_ADDRESS = '0xe7f1725e7734ce288f8367e1bb143e90bb3f0512'
+
+const UNSPLASH_ACCESS_KEY = process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY
+
+const IPFS_BASE_URL = 'https://ipfs.io/ipfs'
+
+const projectId = '2DDHiA47zFkJXtnxzl2jFkyuaoq'
+const projectSecret = '96a91eeafc0a390ab66e6a87f61152aa'
+const projectIdAndSecret = `${projectId}:${projectSecret}`
+
+const ipfs = create({
+  host: 'ipfs.infura.io',
+  port: 5001,
+  protocol: 'https',
+  headers: {
+    authorization: `Basic ${Buffer.from(projectIdAndSecret).toString(
+      'base64'
+    )}`,
+  },
+})
 
 const NftIndex: NextPage = () => {
-  const { data: session, status } = useSession()
+  const [nftUri, setNftUri] = useState('')
+
+  const hasNftUri = Boolean(nftUri)
+
+  const { data: session } = useSession()
   const address = session?.user?.name
 
-  const CONTRACT_ADDRESS = '0xe7f1725e7734ce288f8367e1bb143e90bb3f0512'
-
-  const { data, isError, isLoading } = useContractRead({
+  const { data: nftBalanceData } = useContractRead({
     addressOrName: CONTRACT_ADDRESS,
     contractInterface: YourNFT.abi,
     functionName: 'balanceOf',
     args: address,
   })
 
+  const { data: nftTokenUri } = useContractRead({
+    addressOrName: CONTRACT_ADDRESS,
+    contractInterface: YourNFT.abi,
+    functionName: 'tokenURI',
+    args: '3',
+  })
+
+  // console.log('nftBalanceData', nftBalanceData)
+  // console.log('nftTokenUri', nftTokenUri)
+
   const { config } = usePrepareContractWrite({
     addressOrName: CONTRACT_ADDRESS,
     contractInterface: YourNFT.abi,
     functionName: 'safeMint',
-    args: [address, 'https://github.com/ChangoMan/nextjs-ethereum-starter'],
+    args: [address, nftUri],
+    enabled: hasNftUri,
   })
 
-  const { write } = useContractWrite(config)
+  const { data, write } = useContractWrite(config)
+
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+    onSuccess(data) {
+      console.log('success data', data)
+      setNftUri('')
+    },
+  })
+
+  const mintItem = useCallback(async () => {
+    const fetchImage = async () => {
+      const response = await fetch(
+        `https://api.unsplash.com/photos/random/?client_id=${UNSPLASH_ACCESS_KEY}`
+      )
+
+      if (!response.ok) {
+        throw Error('Error with fetch')
+      }
+
+      const data = await response.json()
+      return data
+    }
+
+    try {
+      // const photos = await fetchImage()
+
+      const tokenuri = generateTokenUri()
+      // const uploaded = await ipfs.add('cool')
+
+      // // This will trigger the useEffect to run the `write()` function.
+      // setNftUri(`${IPFS_BASE_URL}/${uploaded.path}`)
+    } catch (error) {
+      console.log('error', error)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (hasNftUri && write) {
+      write()
+    }
+  }, [hasNftUri, write])
 
   return (
     <Layout>
       <Heading as="h1" mb="8">
         NftIndex
       </Heading>
-      <Button colorScheme="teal" onClick={() => write?.()}>
+      <Button colorScheme="teal" onClick={mintItem}>
         Mint NFT
       </Button>
     </Layout>
---
+++ frontend/utils/generateTokenUri.ts
@@ -0,0 +1,3 @@
+export const generateTokenUri = (data) => {
+  return {}
+}
